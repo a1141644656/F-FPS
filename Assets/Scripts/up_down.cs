ac20c43cd6eca8b4fe353fd41dae2b46b99eed3d
CWB <1141644656@qq.com>
2019-04-08 10:15:42 +0800

    add

---
+++ Assets/Scripts/up_down.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class up_down : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = transform.localPosition.y;
+        Debug.Log(height);
+        if(height > -2.6f)
+        {
+            height = height - 0.1f;
+            Debug.Log(height + " if");
+        }
+        else
+        {
+            height = 2.5f;
+            Debug.Log(height + " else");
+        }
+        transform.localPosition.Set(transform.localPosition.x, height, transform.localPosition.z);
+    }
+}
